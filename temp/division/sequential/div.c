e4a001877e3b6ef2512c8412306f12e6f657a8a1
aske0778 <aske.nord.raahauge@gmail.com>
2025-02-03 18:53:05 +0100

    updates to div

--- temp/division/sequential/div.c
+++ temp/division/sequential/div.c
@@ -12,6 +12,31 @@ typedef digit_t *bigint_t;
 typedef uint32_t prec_t;
 
 
+
+/**
+ * @brief Checks if a bigint_t is equal to zero
+ */
+bool ez(bigint u, prec_t m) {
+    for (int i = 0; i < m; i++) {
+        if (u[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * @brief Checks if two bigint_ts are equal
+ */
+bool eq(bigint_t u, bigint_t v, prec_t m) {
+    for (int i = 0; i < m; i++) {
+        if (u[i] != v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 /**
  * @brief Initializes a bigint_t with m digits
  * 
@@ -26,33 +51,71 @@ bigint_t init(prec_t m) {
     return retval;
 }
 
-
-
+/**
+ * @brief The precision of the bigint_t
+ */
+prec_t prec(bigint_t u, prec_t m) {
+    prec_t acc = 0;
+    for (int i = 0; i < m; i++) {
+        if (u[i] != 0) {
+            acc = i + 1;
+        }
+    }
+    return acc + 1;
+}
 
 /**
  * @brief Shifts a bigint_t to the left or right depending on sign of n
  * 
  * @param n The sign and number of shifts
  * @param u The input bigint_t
+ * @param v bigint_t where result is stored
  * @param m The number of digits in u
- * 
- * @return bigint_t The shifted bigint_t
  */
-bigint_t shift(int n, bigint_t u, prec_t m) {
-    bigint_t v = init(m);
-    if (n >= 0) {
+void shift(int n, bigint_t u, bigint_t v, prec_t m) {
+    if (n >= 0) {   // Right shift
         for (int i = m - 1; i >= 0; i--) {
             int offset = i - n;
             v[i] = (offset >= 0) ? u[offset] : 0;
         }
-    } else {
+    } else {        // Left shift
         for (int i = 0; i < m; i++) {
             int offset = i - n;
             v[i] = (offset < m) ? u[offset] : 0;
         }
     }
-    return v;
 }
 
+
+/**
+ * @brief Uses gmp to multiply two bigint_ts
+ * 
+ * @param u First bigint_t
+ * @param v Second bigint_t
+ * @param w bigint_t where result is stored
+ * @param m The number of digits in u and v
+ */
+void mult(bigint_t u, bigint_t v, bigint_t w, prec_t m) {
+    mpz_t a; mpz_t b; mpz_t r;
+    mpz_init(a); mpz_init(b); mpz_init(r);
+
+    mpz_import(a, m, -1, sizeof(digit_t), 0, 0, u);
+    mpz_import(b, m, -1, sizeof(digit_t), 0, 0, v);
+    mpz_mul(r, a, b);
+
+    set(w, 0, m);
+    mpz_export(w, NULL, -1, sizeof(digit_t), 0, 0, r);
+
+    mpz_clear(a); mpz_clear(b); mpz_clear(r);
+}
+
+
+// void powdiff(bigint_t v, bigint_t w, int h, int l, bigint_t B, prec_t m) {
+//     int L = prec(v, m) + prec(w, m) - l + 1;
+//     if (v == 0 || w == 0 || L >= h) {
+//         return 
+//     }
+
+// }
 
 
