7a8be5fd63ff0ecb2f2b16a72e3da0c1ed61c1c4
Aske <aske.nord.raahauge@gmail.com>
2025-02-04 10:34:55 +0100

    did some stuff

--- temp/division/sequential/div.c
+++ temp/division/sequential/div.c
@@ -6,23 +6,36 @@
 
 
 
-
 typedef uint32_t digit_t;
 typedef digit_t *bigint_t;
 typedef uint32_t prec_t;
+typedef int bool;
 
 
+/**
+ * @brief Returns the minimum of two integers
+ */
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+/**
+ * @brief Returns the maximum of two integers
+ */
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
 /**
  * @brief Checks if a bigint_t is equal to zero
  */
-bool ez(bigint u, prec_t m) {
+bool ez(bigint_t u, prec_t m) {
     for (int i = 0; i < m; i++) {
         if (u[i] != 0) {
-            return false;
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
 
 /**
@@ -31,10 +44,10 @@ bool ez(bigint u, prec_t m) {
 bool eq(bigint_t u, bigint_t v, prec_t m) {
     for (int i = 0; i < m; i++) {
         if (u[i] != v[i]) {
-            return false;
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
 
 /**
@@ -86,6 +99,49 @@ void shift(int n, bigint_t u, bigint_t v, prec_t m) {
     }
 }
 
+/**
+ * @brief Uses gmp to add two bigint_ts
+ * 
+ * @param u First bigint_t
+ * @param v Second bigint_t
+ * @param w bigint_t where result is stored
+ * @param m The number of digits in u and v
+ */
+void add(bigint_t u, bigint_t v, bigint_t w, prec_t m) {
+    mpz_t a; mpz_t b; mpz_t r;
+    mpz_init(a); mpz_init(b); mpz_init(r);
+
+    mpz_import(a, m, -1, sizeof(digit_t), 0, 0, u);
+    mpz_import(b, m, -1, sizeof(digit_t), 0, 0, v);
+    mpz_add(r, a, b);
+
+    set(w, 0, m);
+    mpz_export(w, NULL, -1, sizeof(digit_t), 0, 0, r);
+
+    mpz_clear(a); mpz_clear(b); mpz_clear(r);
+}
+
+/**
+ * @brief Uses gmp to subtract two bigint_ts
+ * 
+ * @param u First bigint_t
+ * @param v Second bigint_t
+ * @param w bigint_t where result is stored
+ * @param m The number of digits in u and v
+ */
+void sub(bigint_t u, bigint_t v, bigint_t w, prec_t m) {
+    mpz_t a; mpz_t b; mpz_t r;
+    mpz_init(a); mpz_init(b); mpz_init(r);
+
+    mpz_import(a, m, -1, sizeof(digit_t), 0, 0, u);
+    mpz_import(b, m, -1, sizeof(digit_t), 0, 0, v);
+    mpz_sub(r, a, b);
+
+    set(w, 0, m);
+    mpz_export(w, NULL, -1, sizeof(digit_t), 0, 0, r);
+
+    mpz_clear(a); mpz_clear(b); mpz_clear(r);
+}
 
 /**
  * @brief Uses gmp to multiply two bigint_ts
@@ -109,13 +165,84 @@ void mult(bigint_t u, bigint_t v, bigint_t w, prec_t m) {
     mpz_clear(a); mpz_clear(b); mpz_clear(r);
 }
 
-
-// void powdiff(bigint_t v, bigint_t w, int h, int l, bigint_t B, prec_t m) {
+// /**
+//  * @brief Calculates B^h-v*w and returns the result in B
+//  * 
+//  * @param v 
+//  * @param w 
+//  * @param h 
+//  * @param l 
+//  * @param B Returns value in this bigint_t
+//  * @param m 
+//  * @return int 
+//  */
+// int powdiff(bigint_t v, bigint_t w, int h, int l, bigint_t B, prec_t m) {
 //     int L = prec(v, m) + prec(w, m) - l + 1;
-//     if (v == 0 || w == 0 || L >= h) {
-//         return 
+//     if (ez(v,m) || ez(w,m) || L >= h) {
+//         bigint_t Bh = init(m);
+//         shift(h, B, Bh, m);
+//         mult(v, w, B, m);
+//         sub(Bh, B, B, m);
+//         free(Bh);
+//     } else {
+//         bigint_t P = multmod();
+//         if (ez(P,m)) { return 0; }
+//         else if () {
+
+//         } else {
+//             bigint_t BL = init(m);
+//             shift(L, B, BL, m);     // In prototype they do shift(L, BL, BL, m) where BL only contains single 1
+//             sub(BL, P, )
+//         }
 //     }
 
 // }
 
 
+
+/**
+ * @brief Refines accuracy of shifted inverse
+ * 
+ * @param v Input divisor
+ * @param h The precision of the input divisor
+ * @param k 
+ * @param w Approximation of quotient
+ * @param l Current number of digits in w
+ * @param m The number of digits in v
+ */
+void refine1(bigint_t v, int h, int k, bigint_t w, int l, prec_t m) {
+    int g = 1;
+    h = h + g;
+    shift(k-k-l, w, w, m);
+    while (h - k > l) {
+        w = step(); // TODO: Implement step
+        l = min(2*l-1, h-k);
+    }
+    shift(-g, w, w, m);
+}
+
+void refine2(bigint_t v, int h, int k, bigint_t w, int l, prec_t m) {
+    int g = 2;
+    shift(g, w, w, m);
+    while (h - k > l) {
+        m = min(h-k+1-l, l);
+        w = step(); // TODO: Implement step
+        shift(-1, w, w, m);
+        l = l + m - 1;
+    }
+    shift(-g, w, w, m);
+}
+
+void refine3(bigint_t v, int h, int k, bigint_t w, int l, prec_t m) {
+    int s;
+    int g = 2;
+    shift(g, w, w, m);
+    while (h - k > l) {
+        m = min(h-k+1-l, l);
+        s = max(0,  k-2*l+1-g);
+        w = step(); // TODO: Implement step
+        shift(-1, w, w, m);
+        l = l + m - 1;
+    }
+    shift(-g, w, w, m);
+}
