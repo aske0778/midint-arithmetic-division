c3c31ed65b089b986b568aeda3ff53522aed8360
dzn816 <dzn816@hendrixfut03fl.unicph.domain>
2025-02-17 14:11:38 +0100

    shift got me acting unwise

--- div/ker-division.cu.h
+++ div/ker-division.cu.h
@@ -7,50 +7,46 @@
 
 
 
+template<int Q>
 __device__ inline void
 BlockwiseShift( const int n,
            const uint32_t* u,
            uint32_t* r,
            const uint32_t m ) {
-    int idx = blockIdx.y * blockDim.x + threadIdx.x;
+    int idx = (blockIdx.y * blockDim.x + threadIdx.x) * Q;
     int offset = idx - n;
     
-    if (idx < m) {
+    #pragma unroll
+    for (int i = idx; i < m; i++) {
         if (n >= 0) {   // Right shift
-            r[idx] = (offset >= 0) ? u[offset] : 0;
+            r[i] = (offset >= 0) ? u[offset] : 0;
         } else {        // Left shift
-            r[idx] = (offset < m) ? u[offset] : 0;
+            r[i] = (offset < m) ? u[offset] : 0;
         }
     }
 }
 
 
+// template<int Q>
 // __device__ inline void
 // BlockwiseShift( const int n,
-//            const digit_t* u,
-//            digit_t* r,
-//            const prec_t m ) {
-//     const unsigned int idx = threadIdx.x;
+//            const uint32_t* u,
+//            uint32_t* r,
+//            const uint32_t m ) {
+//     int idx = blockIdx.y * blockDim.x + threadIdx.x;
+//     int offset = idx - n;
     
-//     if (n >= 0)
-//     { // Right shift
-//         #pragma unroll
-//         for (int i = m - 1; i >= 0; i--)
-//         {
-//             int offset = i - n;
-//             r[i] = (offset >= 0) ? u[offset] : 0;
-//         }
-//     }
-//     else
-//     { // Left shift
-//         #pragma unroll
-//         for (int i = 0; i < m; i++)
-//         {
-//             int offset = i - n;
-//             r[i] = (offset < m) ? u[offset] : 0;
+//     if (idx < m) {
+//         if (n >= 0) {   // Right shift
+//             r[idx] = (offset >= 0) ? u[offset] : 0;
+//         } else {        // Left shift
+//             r[idx] = (offset < m) ? u[offset] : 0;
 //         }
 //     }
 // }
+
+
+
 
 
 
