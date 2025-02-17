dede183e69ec977d0ac2ba24eb6ff1ec1b9ac139
dzn816 <dzn816@hendrixfut03fl.unicph.domain>
2025-02-17 12:25:35 +0100

    v1 ker-division

--- div/ker-division.cu.h
+++ div/ker-division.cu.h
@@ -1,8 +1,8 @@
 #ifndef KERNEL_DIVISION
 #define KERNEL_DIVISION
 
-#include "ker-fft-help.cu.h"
-#include "ker-helpers.cu.h"
+#include "../cuda/ker-fft-help.cu.h"
+#include "../cuda/ker-helpers.cu.h"
 
 
 
@@ -11,27 +11,45 @@ BlockwiseShift( const int n,
            const digit_t* u,
            digit_t* r,
            const prec_t m ) {
-    const unsigned int idx = threadIdx.x;
+    int idx = blockIdx.y * blockDim.x + threadIdx.x;
+    int offset = idx - n;
     
-    if (n >= 0)
-    { // Right shift
-        #pragma unroll
-        for (int i = m - 1; i >= 0; i--)
-        {
-            int offset = i - n;
-            r[i] = (offset >= 0) ? u[offset] : 0;
-        }
-    }
-    else
-    { // Left shift
-        #pragma unroll
-        for (int i = 0; i < m; i++)
-        {
-            int offset = i - n;
-            r[i] = (offset < m) ? u[offset] : 0;
+    if (idx < m) {
+        if (n >= 0) {   // Right shift
+            r[idx] = (offset >= 0) ? u[offset] : 0;
+        } else {        // Left shift
+            r[idx] = (offset < m) ? u[offset] : 0;
         }
     }
 }
+
+
+// __device__ inline void
+// BlockwiseShift( const int n,
+//            const digit_t* u,
+//            digit_t* r,
+//            const prec_t m ) {
+//     const unsigned int idx = threadIdx.x;
+    
+//     if (n >= 0)
+//     { // Right shift
+//         #pragma unroll
+//         for (int i = m - 1; i >= 0; i--)
+//         {
+//             int offset = i - n;
+//             r[i] = (offset >= 0) ? u[offset] : 0;
+//         }
+//     }
+//     else
+//     { // Left shift
+//         #pragma unroll
+//         for (int i = 0; i < m; i++)
+//         {
+//             int offset = i - n;
+//             r[i] = (offset < m) ? u[offset] : 0;
+//         }
+//     }
+// }
 
 
 
