4385a46cc81212db2e0b1d200a7aa3afcc163b2a
dzn816 <dzn816@hendrixfut03fl.unicph.domain>
2025-02-17 15:07:42 +0100

    shift seems to be working

--- div/ker-division.cu.h
+++ div/ker-division.cu.h
@@ -13,37 +13,20 @@ BlockwiseShift( const int n,
            const uint32_t* u,
            uint32_t* r,
            const uint32_t m ) {
-    int idx = (blockIdx.y * blockDim.x + threadIdx.x) * Q;
-    int offset = idx - n;
     
     #pragma unroll
-    for (int i = idx; i < m; i++) {
-        if (n >= 0) {   // Right shift
-            r[i] = (offset >= 0) ? u[offset] : 0;
-        } else {        // Left shift
-            r[i] = (offset < m) ? u[offset] : 0;
+    for (int i = 0; i < Q; i++) {
+        int idx = i * blockDim.x + threadIdx.x;
+        int offset = idx - n;
+        if (idx < m) {
+            if (n >= 0) {   // Right shift
+                r[idx] = (offset >= 0) ? u[offset] : 0;
+            } else {        // Left shift
+                r[idx] = (offset < m) ? u[offset] : 0;
+            }
         }
     }
 }
-
-
-// template<int Q>
-// __device__ inline void
-// BlockwiseShift( const int n,
-//            const uint32_t* u,
-//            uint32_t* r,
-//            const uint32_t m ) {
-//     int idx = blockIdx.y * blockDim.x + threadIdx.x;
-//     int offset = idx - n;
-    
-//     if (idx < m) {
-//         if (n >= 0) {   // Right shift
-//             r[idx] = (offset >= 0) ? u[offset] : 0;
-//         } else {        // Left shift
-//             r[idx] = (offset < m) ? u[offset] : 0;
-//         }
-//     }
-// }
 
 
 
--- div/tests/blockwise-shift-test.cu
+++ div/tests/blockwise-shift-test.cu
@@ -7,52 +7,33 @@
 // #include "../sequential/div.h"
 #include "../../cuda/helper.h"
 
-
 __global__ void CallShift(
     const int n,
     const uint32_t* u,
     uint32_t* r,
     const uint32_t m) {
-        BlockwiseShift(n, u, r, m);
+        BlockwiseShift<2>(n, u, r, m);
     }
 
-
-// template<class uint_t>  // m is the size of the big word in Base::uint_t units
-// void testBasicOps ( int n, int runs ) {    
-//     uint_t* d_as;
-//     size_t mem_size_nums = n * sizeof(uint_t);
-    
-//     cudaMalloc((void**) &d_as, mem_size_nums);
-    
-//     const size_t B = 256;
-    
-//     // timing instrumentation shifting
-//     {
-//         CallShift<<<1, B>>>(n, d_as);
-//         cudaDeviceSynchronize();
-//         gpuAssert( cudaPeekAtLastError() );
-    
-    
-//         uint64_t elapsed;
-//         struct timeval t_start, t_end, t_diff;
-//         gettimeofday(&t_start, NULL); 
-        
-//         for(int i=0; i<runs; i++) {
-//             additionKer<uint_t><<< (n+B-1)/B, B >>>(n, d_as);
-//         }
-        
-//         cudaDeviceSynchronize();
-
-//         gettimeofday(&t_end, NULL);
-//         timeval_subtract(&t_diff, &t_end, &t_start);
-//         elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / runs;
-
-//         gpuAssert( cudaPeekAtLastError() );
-
-//         printf( "Base Addition runs in: %.2f us\n", (double)elapsed );        
-//     }
-//     cudaFree(d_as);
-// }
+void shift(int n, uint32_t* u, uint32_t* r, uint32_t m)
+{
+    if (n >= 0)
+    { // Right shift
+        for (int i = m - 1; i >= 0; i--)
+        {
+            int offset = i - n;
+            r[i] = (offset >= 0) ? u[offset] : 0;
+        }
+    }
+    else
+    { // Left shift
+        for (int i = 0; i < m; i++)
+        {
+            int offset = i - n;
+            r[i] = (offset < m) ? u[offset] : 0;
+        }
+    }
+}
 
 
 
@@ -66,12 +47,16 @@ int main(int argc, char* argv[]) {
     uint32_t m = 10;
     int size = m * sizeof(uint32_t);
 
-    uint32_t u[10] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    // uint32_t u[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    uint32_t* u = (uint32_t*)malloc(size);
     uint32_t* v = (uint32_t*)malloc(size);
     uint32_t* v_D;
     cudaMalloc(&v_D, size);
 
+    randomInit<uint32_t>(u, m);
     cudaMemcpy(v_D, u, size, cudaMemcpyHostToDevice);
+
+    shift(6, u, u, m);
 
     int threadsPerBlock = 256;
     CallShift<<<1, threadsPerBlock>>>(6, v_D, v_D, m);
@@ -93,6 +78,7 @@ int main(int argc, char* argv[]) {
     // free(u);
     free(v);
     cudaFree(v_D);
+    printf("shift: VALID\n");
     return 0;
 }
 
