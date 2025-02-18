8fcedc4143686d2bee3a0a1e4209f90fc0c58686
dzn816 <dzn816@hendrixfut03fl.unicph.domain>
2025-02-18 08:52:39 +0100

    working shift

--- div/ker-division.cu.h
+++ div/ker-division.cu.h
@@ -6,28 +6,58 @@
 // #include "../cuda/ker-helpers.cu.h"
 
 
-
-template<int Q>
+template<uint32_t Q>
 __device__ inline void
 BlockwiseShift( const int n,
            const uint32_t* u,
-           uint32_t* r,
+           uint32_t* res,
            const uint32_t m ) {
-    
+
     #pragma unroll
     for (int i = 0; i < Q; i++) {
         int idx = i * blockDim.x + threadIdx.x;
+        
+        if (idx >= m) {
+           continue; 
+        }
+
         int offset = idx - n;
-        if (idx < m) {
-            if (n >= 0) {   // Right shift
-                r[idx] = (offset >= 0) ? u[offset] : 0;
-            } else {        // Left shift
-                r[idx] = (offset < m) ? u[offset] : 0;
-            }
+        if (n >= 0) {   // Right shift
+            res[idx] = (offset >= 0) ? u[offset] : 0;
+        } else {        // Left shift
+            res[idx] = (offset < m) ? u[offset] : 0;
         }
     }
 }
 
+
+
+template<int Q>
+__device__ inline void
+BlockwiseMultD( uint32_t* a,
+                uint32_t  b,
+                uint32_t* v,
+                const uint32_t m ) {
+    uint64_t buf[m];
+
+    #pragma unroll
+    for (int i = 0; i < Q; i++) {
+        int idx = i * blockDim.x + threadIdx.x;
+        buf[idx] = ((uint64_t)a[idx]) * (uint64_t)b;
+    }
+
+    #pragma unroll
+    for (int i = 1; i < Q; i++) {
+        int idx = i * blockDim.x + threadIdx.x;
+        buf[idx + 1] += buf[idx ] >> 32;
+    }
+
+    #pragma unroll
+    for (int i = 0; i < Q; i++) {
+        int idx = i * blockDim.x + threadIdx.x;
+        v[idx] = (uint32_t)buf[idx];
+    }
+}
 
 
 
