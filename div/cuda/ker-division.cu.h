d5c82568a047d9303965d7d28ccae348f4fa150f
dzn816 <dzn816@hendrixfut01fl.unicph.domain>
2025-02-27 14:11:41 +0100

    updated multmod4Reg

--- div/cuda/ker-division.cu.h
+++ div/cuda/ker-division.cu.h
@@ -101,10 +101,10 @@ multd(volatile T *u,
 template <class Base, uint32_t M, uint32_t Q>
 __device__ inline typename Base::uint_t
 multmod4Reg( typename Base::uint_t Arg[Q]
-       , typename Base::uint_t Brg[Q]
-       , uint32_t d
-       , volatile typename Base::uint_t* Ash
-       , volatile typename Base::uint_t* Bsh
+           , typename Base::uint_t Brg[Q]
+           , uint32_t d
+           , volatile typename Base::uint_t* Ash
+           , volatile typename Base::uint_t* Bsh
 ) {
     using uint_t = typename Base::uint_t;
 
