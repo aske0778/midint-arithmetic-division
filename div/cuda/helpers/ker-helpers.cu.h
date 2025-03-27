520b4427b964c407ab911594914f33af1f14fd5f
Aske Nord Raahauge <aske.nord.raahauge@gmail.com>
2025-03-27 16:33:20 +0100

    bench for quo works now real

--- div/cuda/helpers/ker-helpers.cu.h
+++ div/cuda/helpers/ker-helpers.cu.h
@@ -40,6 +40,57 @@ __device__ inline void cpyReg2Sh2Glb(uint32_t AReg[Q], volatile uint32_t* ASh, v
     __syncthreads();
 }
 
+template<class S, uint32_t IPB, uint32_t M, uint32_t Q>
+__device__ inline
+void cpGlb2Reg ( uint32_t ipb, volatile S* shmem, S* ass, S Arg[Q] ) {
+    const uint32_t M_lft = LIFT_LEN(M, Q); 
+    // 1. read from global to shared memory
+    const uint64_t glb_offs = blockIdx.x * (IPB * M);
+    
+    for(int i=0; i<Q; i++) {
+        uint32_t loc_pos_sh = i*(IPB*M_lft/Q) + threadIdx.x;
+        uint32_t r = loc_pos_sh % M_lft;
+
+        uint32_t loc_pos_glb= (loc_pos_sh / M_lft) * M + r;
+        S el = 0;
+        if( (r < M) && (loc_pos_sh / M_lft < ipb) ) {
+            el = ass[glb_offs + loc_pos_glb];
+        }
+        shmem[loc_pos_sh] = el;       
+    }
+    __syncthreads();
+    // 2. read from shmem to regs
+    for(int i=0; i<Q; i++) {
+        Arg[i] = shmem[Q*threadIdx.x + i];
+    }
+}
+
+template<class S, uint32_t IPB, uint32_t M, uint32_t Q>
+__device__ inline
+void cpReg2Glb ( uint32_t ipb, volatile S* shmem , S Rrg[Q], S* rss ) { 
+    const uint32_t M_lft = LIFT_LEN(M, Q);
+    
+    // 1. write from regs to shared memory
+    uint32_t ind_ipb = threadIdx.x / (M_lft/Q);
+    for(int i=0; i<Q; i++) {
+        uint32_t r = (Q*threadIdx.x + i) % M_lft;
+        uint32_t loc_ind = ind_ipb*M + r;
+        if(r < M)
+            shmem[loc_ind] = Rrg[i];
+    }
+
+    __syncthreads();
+
+    // 2. write from shmem to global
+    const uint64_t glb_offs = blockIdx.x * (IPB * M);
+    for(int i=0; i<Q; i++) {
+        uint32_t loc_pos = i*(IPB*M_lft/Q) + threadIdx.x;
+        if(loc_pos < ipb * M) {
+            rss[glb_offs + loc_pos] = shmem[loc_pos];
+        }
+    }
+}
+
 template<class S, uint32_t Q>
 __device__ inline
 void cpyReg2Shm ( S Rrg[Q], volatile S* shmem ) { 
@@ -65,7 +116,6 @@ __device__ inline uint32_t prec(uint32_t u[Q], volatile uint32_t* sh_mem) {
         if (u[i] != 0) {
             atomicMax((uint32_t*)sh_mem, Q * threadIdx.x + i + 1);
          //   atomicMax(sh_mem, Q * threadIdx.x + i + 1);
-            break;
         }
     }
     __syncthreads();    
