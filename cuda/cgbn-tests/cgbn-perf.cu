5ccf2adb5c2bd52e92a15668544e2b777ebc280d
xhd447 <xhd447@hendrixfut01fl.unicph.domain>
2025-04-14 13:53:59 +0200

    cgbn stuff

--- cuda/cgbn-tests/cgbn-perf.cu
+++ cuda/cgbn-tests/cgbn-perf.cu
@@ -1,6 +1,6 @@
 #include "../helper.h"
 #include <cuda.h>
-#include "include/cgbn/cgbn.h"
+#include "CGBN/cgbn.h"
 
 // IMPORTANT:  DO NOT DEFINE TPI OR BITS BEFORE INCLUDING CGBN
 #define TPI  THD_PER_INST // at least 8 words per thread 
@@ -261,6 +261,58 @@ averaged over %d runs: %lu microsecs, Gopsu32/sec: %.2f, Mil-Instances/sec: %.2f
 	//verify_results(instances, num_instances);
 }
 
+void runquo ( const uint32_t num_instances, const uint32_t cuda_block
+  , cgbn_error_report_t *report,  instance_t  *gpuInstances
+  , instance_t  *instances
+) {
+//printf("Running GPU kernel ...\n");
+
+const uint32_t ipb = cuda_block/TPI;
+
+// start timer
+unsigned long int elapsed = 0;
+struct timeval t_start, t_end, t_diff;
+gettimeofday(&t_start, NULL);
+
+// launch with 32 threads per instance, 128 threads (4 instances) per block
+for(int i = 0; i < GPU_RUNS_CMUL; i++)
+kernel_quo<<<(num_instances+ipb-1)/ipb, cuda_block>>>(report, gpuInstances, num_instances);
+cudaDeviceSynchronize();
+
+//end timer
+gettimeofday(&t_end, NULL);
+timeval_subtract(&t_diff, &t_end, &t_start);
+elapsed = (t_diff.tv_sec*1e6+t_diff.tv_usec) / GPU_RUNS_CMUL;
+
+//printf("Average of %d runs: %ld\n", GPU_RUNS_CMUL, elapsed);
+
+gpuAssert( cudaPeekAtLastError() );
+
+const uint32_t m = BITS / 32;
+double runtime_microsecs = elapsed;
+//double num_u32_ops = 4.0 * num_instances * m * m; 
+double num_u32_ops = num_instances * numAd32OpsOfMultInst<uint32_t>(m);
+double gigaopsu32 = num_u32_ops / (runtime_microsecs * 1000);
+
+printf( "CGBN quo (num-instances = %d, num-word-len = %d, total-size: %d), \
+averaged over %d runs: %lu microsecs, Gopsu32/sec: %.2f, Mil-Instances/sec: %.2f\n"
+, num_instances, m, num_instances * m, GPU_RUNS_CMUL
+, elapsed, gigaopsu32, num_instances / runtime_microsecs
+);
+
+// error report uses managed memory, so we sync the device (or stream) and check for cgbn errors
+CUDA_CHECK(cudaDeviceSynchronize());
+CGBN_CHECK(report);
+
+// copy the instances back from gpuMemory
+// printf("Copying results back to CPU, size of instance_t: %d ...\n", sizeof(instance_t));
+CUDA_CHECK(cudaMemcpy(instances, gpuInstances, sizeof(instance_t)*num_instances, cudaMemcpyDeviceToHost));
+
+//printf("Verifying the results ...\n");
+//verify_results(instances, num_instances);
+verifyResults(false, num_instances, instances);
+}
+
 
 int main(int argc, char * argv[]) {
     if (argc != 2) {
@@ -288,6 +340,7 @@ int main(int argc, char * argv[]) {
     runAdd (num_instances, 128, report, gpuInstances, instances);
     runMul (num_instances, 128, report, gpuInstances, instances);
     runPoly(num_instances, 128, report, gpuInstances, instances);
+    runquo(num_instances, 128, report, gpuInstances, instances);
     
 	// clean up
 	free(instances);
